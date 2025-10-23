e84484832de815eada424fbdd08bcd64b94a74b8
995933447 <34621597+995933447@users.noreply.github.com>
2025-10-23 17:58:43 +0200

    Update fmt.go

--- logger/fmts/fmt.go
+++ logger/fmts/fmt.go
@@ -1,7 +1,19 @@
 package fmts
 
+import "github.com/995933447/fastlog/logger"
+
 type Format int
 
 const (
 	FormatText Format = iota
 )
+
+var levelToStdoutColorMap = map[logger.Level]logger.Color{
+	logger.LevelDebug:     logger.ColorLightGreen,
+	logger.LevelInfo:      logger.ColorLightGreen,
+	logger.LevelImportant: logger.ColorBlue,
+	logger.LevelWarn:      logger.ColorGreen,
+	logger.LevelError:     logger.ColorRed,
+	logger.LevelPanic:     logger.ColorRed,
+	logger.LevelFatal:     logger.ColorPurple,
+}
