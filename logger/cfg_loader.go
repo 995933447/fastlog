4890096b2fc180f280a1c38725d2c01732b29dd3
995933447 <34621597+995933447@users.noreply.github.com>
2025-10-28 11:19:54 +0100

    Add files via upload

--- logger/cfg_loader.go
+++ logger/cfg_loader.go
@@ -10,26 +10,26 @@ import (
 )
 
 type LogConf struct {
-	File       FileLogConf
-	AlertLevel string
+	File       FileLogConf `json:"file"`
+	AlertLevel string      `json:"alert_level"`
 }
 
 type FileLogConf struct {
-	FilePrefix                  string
-	MaxFileSizeBytes            int64  // 文件最大容量,字节为单位
-	DefaultLogDir               string // 默认日志目录
-	ExceptionLogDir             string // 异常日志目录
-	BillLogDir                  string // bill日志目录
-	StatLogDir                  string // stat日志目录
-	Level                       string // 日志最小级别
-	DebugMsgMaxLen              int32  // debug日志消息最大长度,-1或者0代表不限制
-	InfoMsgMaxLen               int32  // info日志消息最大长度,-1或者0代表不限制
-	LogDebugBeforeFileSizeBytes int64  // 文件允许写入debug日志的大小阀值,-1代表不限制
-	LogInfoBeforeFileSizeBytes  int64  // 文件允许写入info日志的大小阀值,-1代表不限制
-	FileMaxRemainDays           int    // 文件最大保留天数
-	MaxRemainFileNum            int    // 保留文件数量
-	CompressFrequentHours       int    // 压缩频率小时数
-	CompressAfterReachBytes     int64  // 压缩最小文件大小
+	FilePrefix                  string `json:"file_prefix"`
+	MaxFileSizeBytes            int64  `json:"max_file_size_bytes"`              // 文件最大容量,字节为单位
+	DefaultLogDir               string `json:"default_log_dir"`                  // 默认日志目录
+	ExceptionLogDir             string `json:"exception_log_dir"`                // 异常日志目录
+	BillLogDir                  string `json:"bill_log_dir"`                     // bill日志目录
+	StatLogDir                  string `json:"stat_log_dir"`                     // stat日志目录
+	Level                       string `json:"level"`                            // 日志最小级别
+	DebugMsgMaxLen              int32  `json:"debug_msg_max_len"`                // debug日志消息最大长度,-1或者0代表不限制
+	InfoMsgMaxLen               int32  `json:"info_msg_max_len"`                 // info日志消息最大长度,-1或者0代表不限制
+	LogDebugBeforeFileSizeBytes int64  `json:"log_debug_before_file_size_bytes"` // 文件允许写入debug日志的大小阀值,-1代表不限制
+	LogInfoBeforeFileSizeBytes  int64  `json:"log_info_before_file_size_bytes"`  // 文件允许写入info日志的大小阀值,-1代表不限制
+	FileMaxRemainDays           int    `json:"file_max_remain_days"`             // 文件最大保留天数
+	MaxRemainFileNum            int    `json:"max_remain_file_num"`              // 保留文件数量
+	CompressFrequentHours       int    `json:"compress_frequent_hours"`          // 压缩频率小时数
+	CompressAfterReachBytes     int64  `json:"compress_after_reach_bytes"`       // 压缩最小文件大小
 }
 
 func (f *FileLogConf) GetLevel() Level {
